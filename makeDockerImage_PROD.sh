822ddfc887369c331ed57d8311985abccd96bd08
Ckevyn Omar Ovalle Sequén <covalle@minfin.gob.gt>
2025-10-17 09:01:07 -0600

    (update)  makedockerimage_prod

--- makeDockerImage_PROD.sh
+++ makeDockerImage_PROD.sh
@@ -20,12 +20,12 @@ cd dist
 
 
 # Crear la imagen usando buildah
-buildah --format=docker bud -t clima-laboral-frontend-img --add-host=srv-osnexus01.minfin.gob.gt:172.18.27.115 .
+buildah --format=docker bud -t meet-frontend-img --add-host=srv-osnexus01.minfin.gob.gt:172.18.27.115 .
 
 # Etiquetar la imagen con la versión y el commit SHA para rastreo en PROD
-buildah tag clima-laboral-frontend-img srv-osnexus01.minfin.gob.gt:8006/clima-laboral-frontend-img:${VERSION}_${COMMIT_ID}
-buildah push srv-osnexus01.minfin.gob.gt:8006/clima-laboral-frontend-img:${VERSION}_${COMMIT_ID}
+buildah tag meet-frontend-img srv-osnexus01.minfin.gob.gt:8006/meet-frontend-img:${VERSION}_${COMMIT_ID}
+buildah push srv-osnexus01.minfin.gob.gt:8006/meet-frontend-img:${VERSION}_${COMMIT_ID}
 
 # Mantener "prod-latest" actualizado
-buildah tag srv-osnexus01.minfin.gob.gt:8006/clima-laboral-frontend-img:${VERSION}_${COMMIT_ID} srv-osnexus01.minfin.gob.gt:8006/clima-laboral-frontend-img:latest
-buildah push srv-osnexus01.minfin.gob.gt:8006/clima-laboral-frontend-img:latest
+buildah tag srv-osnexus01.minfin.gob.gt:8006/meet-frontend-img:${VERSION}_${COMMIT_ID} srv-osnexus01.minfin.gob.gt:8006/meet-frontend-img:latest
+buildah push srv-osnexus01.minfin.gob.gt:8006/meet-frontend-img:latest
